529b0bc014eb03d0903788dbd5b6aa35d6a00aaf
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-11-01 22:22:44 -0500

    Week 5 - rest of materials

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
 #write your code here
+def echo(input)
+    input
+end
+   
+def shout(string)
+    string.upcase
+end
+   
+def repeat(string, times = 2)
+    ([string] * times).join(' ')
+end
+   
+def start_of_word(string, num)
+    string.slice(0, num)
+end
+   
+def first_word(string)
+    string.split(" ").first
+end
+   
+
+def titleize(string)
+    capArray = []
+
+    array = string.split(" ")
+        array.each do |word|  
+
+        if  word == array[-1] 
+            capArray.push(word.capitalize ) 
+
+        elsif word.length < 5 
+            capArray.push(word)
+
+        else
+            capArray.push(word.capitalize )   
+        end
+    end
+
+    first = capArray.first.capitalize
+    capArray[0] = first
+    return capArray.join (" ")
+end
